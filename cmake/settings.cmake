a4c40edf48e3c153c13f1e171363a69571fe746d
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2023-02-01 21:43:22 +0100

    changed how code coverage works

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -1,44 +1,30 @@
 
 # Project options
-option(BUILD_EXECUTABLE     "Build the project as an executable, rather than a library." OFF)
-option(BUILD_HEADERS_ONLY   "Build the project as a header-only library."                OFF)
-
-# Compiler options
-option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors." OFF)
-
-# Package managers options(Conan, Vcpkg)
-option(ENABLE_CONAN "Enable the Conan package manager for this project." OFF)
-option(ENABLE_VCPKG "Enable the Vcpkg package manager for this project." OFF)
+option(BUILD_EXECUTABLE     "Build the project as an executable."             OFF)
+option(BUILD_HEADERS_ONLY   "Build the project as a header-only library."     OFF)
 
 # Unit testing using GoogleTest and GoogleMock
-option(ENABLE_UNIT_TESTING  "Enable unit tests for the projects (from the `test` subfolder)." ON)
+option(ENABLE_UNIT_TESTING  "Enable unit tests for the projects."             ON)
+
+# Doxygen
+option(ENABLE_DOXYGEN       "Enable Doxygen documentation builds of source."  OFF)
 
 # Clang-Tidy and Clang-Format
-option(ENABLE_CLANG_TIDY    "Enable static analysis with Clang-Tidy."   OFF)
-option(ENABLE_CLANG_FORMAT  "Enable code foramtting with Clang-Format." OFF)
+option(ENABLE_CLANG_TIDY    "Enable static analysis with Clang-Tidy."         OFF)
+option(ENABLE_CLANG_FORMAT  "Enable code foramtting with Clang-Format."       OFF)
+
+# Code coverage options
+option(ENABLE_CODE_COVERAGE "Enable code coverage through GCC or CLANG."      ON)
+
+# Compiler options
+option(WARNINGS_AS_ERRORS   "Treat compiler warnings as errors."              OFF)
+
+# Package managers options(Conan, Vcpkg)
+option(ENABLE_CONAN         "Enable the Conan package manager."               OFF)
+option(ENABLE_VCPKG         "Enable the Vcpkg package manager."               OFF)
 
 # Sanitizers options
 set(USE_SANITIZER "" CACHE STRING "Address, Memory, MemoryWithOrigins, Undefined, Thread, Leak, CFI")
-# Code coverage options
-option(ENABLE_CODE_COVERAGE "Enable code coverage through GCC."       ON)
-
-# Doxygen
-option(ENABLE_DOXYGEN       "Enable Doxygen documentation builds of source." OFF)
-
-# CCCACHE options
-option(ENABLE_CCACHE "Enable the usage of Ccache, in order to speed up rebuild times." ON)
-find_program(CCACHE_FOUND ccache)
-if(CCACHE_FOUND)
-  set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
-  set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
-endif()
-
-# Address Sanitizers options
-option(ENABLE_ASAN "Enable Address Sanitize to detect memory error." OFF)
-if(ENABLE_ASAN)
-    add_compile_options(-fsanitize=address)
-    add_link_options(-fsanitize=address)
-endif()
 
 # Generate compile_commands.json for clang based tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
