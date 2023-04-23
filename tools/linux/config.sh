1f84b4e01a02a58df51de455ec0e907d060505a4
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2023-04-23 09:24:35 +0200

    sources cmake correction, example main.c to test linking

--- tools/linux/config.sh
+++ tools/linux/config.sh
@@ -3,7 +3,8 @@
 # run from root folder
 cmake -S . -B build \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DBUILD_EXECUTABLE=ON \
+    -DCMAKE_TOOLCHAIN_FILE=cmake/gcc-arm-none-eabi.cmake \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=true \
     -DENABLE_CLANG_TIDY=ON \
     -DENABLE_CLANG_FORMAT=ON \
     -DENABLE_DOXYGEN=ON \
