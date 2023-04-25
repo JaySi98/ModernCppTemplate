6a7d2103a3dd532d84288775d7deb2bae7035115
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2023-04-25 21:10:34 +0200

    moved files to cubeMX directory, corrected readme

--- README.md
+++ README.md
@@ -8,33 +8,25 @@ This project is mostly a combination of other templates listed below:
 * https://github.com/TheLartians/ModernCppStarter 
 * https://github.com/pabloariasal/modern-cmake-sample 
 
-
-* https://www.labri.fr/perso/fleury/posts/programming/using-clang-tidy-and-clang-format.html
-* https://github.com/StableCoder/cmake-scripts#sanitizer-builds-sanitizerscmake
-## Features 
-
 ## Requirements 
 In order to be able to work with this template, you will need: 
 * CMake - version 3.15 and above 
-* C Compiler 
+* GCC Compiler 
 * Doxygen - for ability to generate docs
 sudo apt-get install doxygen doxygen-doc graphviz 
-* clang-tidy
-* clang-format
 * cmocka test framework
 
 ## Getting Started 
+1. Change project name in CmakeLists.txt
+2. If used on different MCU: change all files inside cubeMX folder by files and add appropiate changes inside cmake/sources.cmake
+3. Add new header and source files to cmake/sources.cmake
+4. Compile, build and run
 
 ## Commands 
 how to: 
-1. install 
-cmake -S . -B build/ 
-cmake --build <build_directory> --target install --config <desired_config> 
 2. build 
 cmake -S . -B build/ 
 cmake --build build 
-3. remove installed 
-sudo xargs rm < build/install_manifest.txt 
 4. generate documentation
 cmake .. -DENABLE_DOXYGEN=1
 cmake --build . --target doxygen-docs
@@ -42,5 +34,3 @@ xdg-open docs/html/index.html
 5. run tests
 ./build/test/UnitTests
 cd build && ctest -C -VV Debug
-6. format code
-cmake --build build/ --target clang-format
--- cmake/sources.cmake
+++ cmake/sources.cmake
@@ -10,7 +10,7 @@ set(TEST_SOURCES
     mainTest.c
 )
 
-# potential changes here
+# changes regarding MCU
 set(MCU_FAMILY STM32F4xx)
 set(MCU_MODEL STM32F411xE)
 set(CPU_PARAMETERS
@@ -20,38 +20,38 @@ set(CPU_PARAMETERS
     -mfloat-abi=hard)
 
 set(STM32CUBEMX_SOURCES
-    Core/Src/stm32f4xx_it.c
-    Core/Src/sysmem.c
-    Core/Src/stm32f4xx_hal_msp.c
-    Core/Src/syscalls.c
-    Core/Src/system_stm32f4xx.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c
-    Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c
+    cubeMX/Core/Src/stm32f4xx_it.c
+    cubeMX/Core/Src/sysmem.c
+    cubeMX/Core/Src/stm32f4xx_hal_msp.c
+    cubeMX/Core/Src/syscalls.c
+    cubeMX/Core/Src/system_stm32f4xx.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c
+    cubeMX/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c
 )
 
 set(CUBEMX_INCLUDE_DIRECTORIES
-    ${CMAKE_CURRENT_SOURCE_DIR}/Core/Inc
-    ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/${MCU_FAMILY}_HAL_Driver/Inc
-    ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/${MCU_FAMILY}_HAL_Driver/Inc/Legacy
-    ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/CMSIS/Device/ST/${MCU_FAMILY}/Include
-    ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/CMSIS/Include
+    ${CMAKE_CURRENT_SOURCE_DIR}/cubeMX/Core/Inc
+    ${CMAKE_CURRENT_SOURCE_DIR}/cubeMX/Drivers/${MCU_FAMILY}_HAL_Driver/Inc
+    ${CMAKE_CURRENT_SOURCE_DIR}/cubeMX/Drivers/${MCU_FAMILY}_HAL_Driver/Inc/Legacy
+    ${CMAKE_CURRENT_SOURCE_DIR}/cubeMX/Drivers/CMSIS/Device/ST/${MCU_FAMILY}/Include
+    ${CMAKE_CURRENT_SOURCE_DIR}/cubeMX/Drivers/CMSIS/Include
 )
 
-set(STARTUP_SCRIPT      ${CMAKE_CURRENT_SOURCE_DIR}/Core/Startup/startup_stm32f411retx.s)
-set(MCU_LINKER_SCRIPT   ${CMAKE_CURRENT_SOURCE_DIR}/Core/Startup/STM32F411RETX_FLASH.ld) 
+set(STARTUP_SCRIPT      ${CMAKE_CURRENT_SOURCE_DIR}/cubeMX/Core/Startup/startup_stm32f411retx.s)
+set(MCU_LINKER_SCRIPT   ${CMAKE_CURRENT_SOURCE_DIR}/cubeMX/Core/Startup/STM32F411RETX_FLASH.ld) 
