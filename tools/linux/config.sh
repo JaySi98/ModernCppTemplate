6b06f5cc76af58518d0616dc625f289ff8de7635
JaySi98 <jakub.szkiladz@wp.pl>
2023-11-26 11:51:19 +0100

    fixed cmake file

--- tools/linux/config.sh
+++ tools/linux/config.sh
@@ -3,6 +3,7 @@
 # run from root folder
 cmake -S . -B build \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_TOOLCHAIN_FILE=cmake/gcc-arm-none-eabi.cmake \
+    -DENABLE_UNIT_TESTING=OFF \
+    -DWARNINGS_AS_ERRORS=ON \
     -DENABLE_DOXYGEN=ON \
     # -DUSE_SANITIZER=Address,Leak \
