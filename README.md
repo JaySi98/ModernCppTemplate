95683851975aff2dbd8050deb85c19450c2dae79
JaySi98 <jakub.szkiladz@wp.pl>
2023-12-02 21:13:13 +0100

    led blink

--- README.md
+++ README.md
@@ -2,19 +2,20 @@
 
 ### About 
 This project is an attempt to create template for C/C++ projects using modern cmake. It's designed to suit my personal requirements, but it might be useful for other developers too. 
-### Acknowledgements 
-This project is mostly a combination of other templates listed below: 
-* https://github.com/filipdutescu/modern-cpp-template 
-* https://github.com/TheLartians/ModernCppStarter 
-* https://github.com/pabloariasal/modern-cmake-sample 
 
 ## Requirements 
 In order to be able to work with this template, you will need: 
 * CMake - version 3.15 and above 
 * GCC Compiler 
 * Doxygen - for ability to generate docs
-sudo apt-get install doxygen doxygen-doc graphviz 
+    sudo apt-get install doxygen doxygen-doc graphviz 
 * cmocka test framework
+* arm-none-eabi-gcc compiler
+    https://askubuntu.com/questions/1243252/how-to-install-arm-none-eabi-gdb-on-ubuntu-20-04-lts-focal-fossa
+    https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads
+    https://askubuntu.com/questions/1031103/how-can-i-install-gdb-arm-none-eabi-on-ubuntu-18-04-bionic-beaver
+* stlink-tools
+    sudo apt install stlink-tools
 
 ## Getting Started 
 1. Change project name in CmakeLists.txt
@@ -24,19 +25,31 @@ sudo apt-get install doxygen doxygen-doc graphviz
 
 ## Commands 
 how to: 
+
 2. build 
 cmake -S . -B build/ 
 cmake --build build 
+
 4. generate documentation
 cmake .. -DENABLE_DOXYGEN=1
 cmake --build . --target doxygen-docs
 xdg-open docs/html/index.html 
+
 5. run tests
 ./build/test/UnitTests
 cd build && ctest -C -VV Debug
+
 6. upload to board
 st-flash write build/Template.bin 0x8000000
+
 7. probe board
     st-info --probe
+
 8. read binary from board
     st-flash read dummy.bin 0 0xFFFF
+
+
+## Datasheets
+1.Board Manual:
+- 23 - LEDs
+- 32 - Pinout
--- src/main.c
+++ src/main.c
@@ -1,16 +1,107 @@
 #include "main.h"
+#include "stm32f4xx_hal.h"
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 
 int main(void)
 {
-	while (1)
-	{
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
 
-	}
+  while (1)
+  {
+
+    if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET)
+    {
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+    }
+    else
+    {
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+    }
+
+    // // Toggle the LED
+    // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    // // Delay for 1 second
+    // HAL_Delay(500);
+  }
+
 }
 
-/**
- * @brief  This function is executed in case of error occurrence.
- */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  // Configure the main internal regulator output voltage
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  // Initializes the RCC Oscillators according to the specified parameters
+  // in the RCC_OscInitTypeDef structure.
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 64;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  // Initializes the CPU, AHB and APB busses clocks
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV4;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  // FLASH_LATENCY_5
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_GPIO_Init(void)
+{
+  // Configure GPIO pin PA5(led) as output
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  // Configure GPIO pin PB6 as output
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStructLED = {0};
+  GPIO_InitStructLED.Pin = GPIO_PIN_7;
+  GPIO_InitStructLED.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStructLED.Pull = GPIO_NOPULL;
+  GPIO_InitStructLED.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStructLED);
+
+  // Configure GPIO pin PC13(button) as input
+  GPIO_InitTypeDef GPIO_InitStructButton = {0};
+  GPIO_InitStructButton.Pin = GPIO_PIN_13;
+  GPIO_InitStructButton.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStructButton.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStructButton);
+
+}
+
 void Error_Handler(void)
 {
   __disable_irq();
@@ -21,12 +112,7 @@ void Error_Handler(void)
 }
 
 #ifdef USE_FULL_ASSERT
-/**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- */
+
 void assert_failed(uint8_t *file, uint32_t line)
 {
 
